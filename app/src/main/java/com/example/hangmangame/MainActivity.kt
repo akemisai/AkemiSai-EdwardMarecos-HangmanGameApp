1d7be23fab8c7097823e1f773ce17bc178f641fd
Edward M <emarecos@bu.edu>
2024-09-29 17:25:13 -0400

    cur app

--- app/src/main/java/com/example/hangmangame/MainActivity.kt
+++ app/src/main/java/com/example/hangmangame/MainActivity.kt
@@ -4,13 +4,18 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Button
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import com.example.hangmangame.ui.theme.HangmanGameTheme
 
 class MainActivity : ComponentActivity() {
@@ -20,8 +25,7 @@ class MainActivity : ComponentActivity() {
         setContent {
             HangmanGameTheme {
                 Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Android",
+                    Hangman(
                         modifier = Modifier.padding(innerPadding)
                     )
                 }
@@ -31,17 +35,29 @@ class MainActivity : ComponentActivity() {
 }
 
 @Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
+fun Hangman(modifier: Modifier = Modifier) {
+
+    Column(
         modifier = modifier
-    )
+            .fillMaxSize()
+            .padding(16.dp),
+        verticalArrangement = Arrangement.Center,
+        horizontalAlignment = Alignment.CenterHorizontally
+    ) {
+        Button(onClick = { /*TODO*/ }) {
+            Text(text = "Reset Game")
+        }
+    }
 }
 
 @Preview(showBackground = true)
 @Composable
-fun GreetingPreview() {
+fun HangmanPreview() {
     HangmanGameTheme {
-        Greeting("Android")
+        Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
+            Hangman(
+                modifier = Modifier.padding(innerPadding)
+            )
+        }
     }
 }
